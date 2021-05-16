0e225f8a4adcb9d32890286b7624e8f3c9718db1
anguswu <ajdfajdfl2003@gmail.com>
2021-05-16 16:36:29 +0800

    feat: initialize task06

---
+++ Week_02/task06/src/test/java/idv/angus/java003/task06/Task06ApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.angus.java003.task06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
