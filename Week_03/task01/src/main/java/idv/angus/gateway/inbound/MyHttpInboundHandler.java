6ca9bf1eee52119ef0067dbd37a7f3cd7e631352
anguswu <ajdfajdfl2003@gmail.com>
2021-05-23 15:43:45 +0800

    refactor: [task01] handle http status 200, and another http status

--- Week_03/task01/src/main/java/idv/angus/gateway/inbound/MyHttpInboundHandler.java
+++ Week_03/task01/src/main/java/idv/angus/gateway/inbound/MyHttpInboundHandler.java
@@ -4,6 +4,7 @@ import idv.angus.gateway.outbound.MyHttpOutboundHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.util.ReferenceCountUtil;
 
 public class MyHttpInboundHandler extends ChannelInboundHandlerAdapter {
     private final MyHttpOutboundHandler outBoundHandler;
@@ -19,7 +20,11 @@ public class MyHttpInboundHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        final FullHttpRequest fullHttpRequest = (FullHttpRequest) msg;
-        outBoundHandler.handle(fullHttpRequest, ctx);
+        try {
+            final FullHttpRequest fullHttpRequest = (FullHttpRequest) msg;
+            outBoundHandler.handle(fullHttpRequest, ctx);
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
     }
 }
--- Week_03/task01/src/main/java/idv/angus/gateway/outbound/MyHttpOutboundHandler.java
+++ Week_03/task01/src/main/java/idv/angus/gateway/outbound/MyHttpOutboundHandler.java
@@ -3,36 +3,47 @@ package idv.angus.gateway.outbound;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.http.DefaultFullHttpResponse;
-import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.codec.http.FullHttpResponse;
-import io.netty.handler.codec.http.HttpUtil;
+import io.netty.handler.codec.http.*;
 import lombok.extern.log4j.Log4j2;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.concurrent.FutureCallback;
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.impl.nio.reactor.IOReactorConfig;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
-import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
 import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 @Log4j2
 public class MyHttpOutboundHandler {
-    private final CloseableHttpAsyncClient httpclient = HttpAsyncClients.createDefault();
     private final ExecutorService proxy;
     private final String backendUrl;
+    private final CloseableHttpAsyncClient httpclient;
 
     public MyHttpOutboundHandler(String backendUrl) {
         this.backendUrl = backendUrl;
-        proxy = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        final int nThreads = Runtime.getRuntime().availableProcessors();
+        proxy = Executors.newFixedThreadPool(nThreads);
+
+        IOReactorConfig ioConfig = IOReactorConfig.custom()
+                .setConnectTimeout(1000)
+                .setSoTimeout(1000)
+                .setIoThreadCount(nThreads)
+                .setRcvBufSize(32 * 1024)
+                .build();
+        httpclient = HttpAsyncClients.custom().setMaxConnTotal(40)
+                .setMaxConnPerRoute(8)
+                .setDefaultIOReactorConfig(ioConfig)
+                .setKeepAliveStrategy((response, context) -> 6000)
+                .build();
+        httpclient.start();
     }
 
     public void handle(FullHttpRequest inbound, ChannelHandlerContext ctx) {
@@ -62,21 +73,29 @@ public class MyHttpOutboundHandler {
         });
     }
 
-    private void handleResponse(final FullHttpRequest inbound, ChannelHandlerContext ctx, final HttpResponse endpointResponse) {
+    private void handleResponse(final FullHttpRequest inbound, final ChannelHandlerContext ctx, final HttpResponse endpointResponse) {
         FullHttpResponse response = null;
         try {
-            byte[] body = EntityUtils.toByteArray(endpointResponse.getEntity());
-            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(body));
+            // 只有 Http status code = 200 才有回傳內容
+            // 其餘 後端 API 是什麼回傳，Proxy 就怎麼回傳
+            if (200 == endpointResponse.getStatusLine().getStatusCode()) {
+                byte[] body = EntityUtils.toByteArray(endpointResponse.getEntity());
+                response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(body));
+                response.headers().setInt("Content-Length", Integer.parseInt(endpointResponse.getFirstHeader("Content-Length").getValue()));
+            } else {
+                response = new DefaultFullHttpResponse(HTTP_1_1, HttpResponseStatus.valueOf(endpointResponse.getStatusLine().getStatusCode()));
+                response.headers().setInt("Content-Length", 0);
+            }
             response.headers().set("Content-Type", "application/json");
-            response.headers().setInt("Content-Length", response.content().readableBytes());
         } catch (IOException e) {
             log.error("handle response failed", e);
+            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+            ctx.close();
         } finally {
             if (inbound != null) {
                 if (!HttpUtil.isKeepAlive(inbound)) {
                     ctx.write(response).addListener(ChannelFutureListener.CLOSE);
                 } else {
-                    response.headers().set(CONNECTION, KEEP_ALIVE);
                     ctx.write(response);
                 }
             }
