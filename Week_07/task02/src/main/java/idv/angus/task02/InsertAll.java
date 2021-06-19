2fca855d824dae0f84f7f816bba8f7d786759e41
anguswu <ajdfajdfl2003@gmail.com>
2021-06-19 23:08:35 +0800

    chore: [Week 07][task02] init project

---
+++ Week_07/task02/src/main/java/idv/angus/task02/InsertAll.java
@@ -0,0 +1,12 @@
+package idv.angus.task02;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class InsertAll {
+    private static final MyDataSource dataSource = new MyDataSource();
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Week_07/task02/src/main/java/idv/angus/task02/MyDataSource.java
@@ -0,0 +1,28 @@
+package idv.angus.task02;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+class MyDataSource {
+    private static final String url = "jdbc:mysql://localhost:3306/test";
+    private static final String user = "root";
+    private static final String password = "";
+    private final HikariDataSource ds;
+
+    public MyDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(user);
+        config.setPassword(password);
+        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        config.setMaximumPoolSize(5);
+        ds = new HikariDataSource(config);
+    }
+
+    public Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+}
